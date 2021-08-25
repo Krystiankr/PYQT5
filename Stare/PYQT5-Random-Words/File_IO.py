cee37acd5bc915002c30c3c04b7c0f6098ee3360
Krystian <krystian.krecikk@wp.pl>
2021-08-25 20:33:18 +0200

    rwr

--- Stare/PYQT5-Random-Words/File_IO.py
+++ Stare/PYQT5-Random-Words/File_IO.py
@@ -14,7 +14,7 @@ class File:
 
     def return_lines(self):
         return self.file.readlines()
-# 222 222
+# 222 222 xx 3
     def write_lines(self, lines):
         self.file.writelines(lines)
 
