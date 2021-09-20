946b47386a777db9c5fa2cb35d667beedbdbd9ee
Krystian Kręcik <56454269+Krystiankr@users.noreply.github.com>
2021-09-21 00:53:48 +0200

    Add files via upload

--- control_files/Graph.py
+++ control_files/Graph.py
@@ -1,9 +1,6 @@
-import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton
 
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QLabel, QPushButton
-
-import matplotlib
 import pandas as pd
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 import matplotlib.pyplot as plt
@@ -11,56 +8,59 @@ import matplotlib
 
 matplotlib.use("Qt5Agg")
 
+
 class Graph(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.df = pd.DataFrame({"A": [0]})
 
-        self.canvas = FigureCanvasQTAgg(plt.Figure(figsize=(7, 7), facecolor='grey'))
+        self.canvas = FigureCanvasQTAgg(plt.Figure(figsize=(3, 3),
+                                                   facecolor=(240/255, 240/255, 240/255),
+                                                   tight_layout=True))
 
         self.increase = QPushButton("increase")
         self.decrease = QPushButton("decrease")
         self.increase.clicked.connect(lambda: self.add_item(1))
         self.decrease.clicked.connect(lambda: self.add_item(-1))
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.increase, alignment=QtCore.Qt.AlignLeft)
-        layout.addWidget(self.decrease, alignment=QtCore.Qt.AlignLeft)
-
         self.value = 0
         self.ax = self.canvas.figure.subplots()
         self.bar = None
+        self.last = 0
+        self.last2 = 0
 
-        widget = QWidget()
-        widget.setLayout(layout)
+        fontsize = 10
+        color = 'steelblue'
 
-        self.ax.set_title("Statistic progress", fontsize=20, color='azure')
-        self.ax.set_xlabel('number of solved words', fontsize=20, color='gold')
-        self.ax.set_ylabel('difference correctly/badly', fontsize=20, color='gold')
-        self.ax.set_facecolor('powderblue')
+        self.ax.set_title("Statistic progress", fontsize=fontsize+5, color='navy')
+        self.ax.set_xlabel('number of solved words', fontsize=fontsize, color=color)
+        self.ax.set_ylabel('difference correctly/badly', fontsize=fontsize, color=color)
+        self.ax.set_facecolor('aliceblue')
 
-        self.setCentralWidget(widget)
-        self.show()
+    def return_canvas(self):
+        return self.canvas
 
     def add_item(self, val):
-        max = self.df.index.max() + 1
+        max_value = self.df.index.max() + 1
         self.value += val
 
-        self.ax.set_xlim([max-10, max+2])
-        self.ax.set_ylim([self.value-6, self.value+6])
+      #  if val == -1:
+      #      self.last = max_value - 10
+      #      self.last2 = self.value - 6
+      #      self.ax.set_xlim([max_value - 10, max_value + 2])
+      #      self.ax.set_ylim([self.value - 6, self.value + 6])
+     #   else:
+      #      self.ax.set_xlim([self.last, max_value + 2])
+      #      self.ax.set_ylim([self.last2, self.value + 6])
+      #     self.last -= 0.3
+        self.ax.set_xlim([0, max_value + 2])
+     #   self.ax.set_ylim([0, self.value + 6])
 
-        self.df.loc[max] = [self.value]
+        self.df.loc[max_value] = [self.value]
         print(self.df)
-
-        if val == 1:
-            color = 'limegreen'
-        else:
-            color = 'firebrick'
-
         if self.bar:
             del self.bar
-        self.bar = self.ax.plot("A", data=self.df, color=color)
+        self.bar = self.ax.plot("A", data=self.df)
         plt.tight_layout()
         self.canvas.draw()
