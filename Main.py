e5b6d204d7b9d152ec3b98d92ab0ee245d825ab6
krystiankr <162847@student.uwm.edu.pl>
2022-08-17 22:33:11 +0200

    Update progress bar & timer & simulate key pressed 1, 2, 3

--- Main.py
+++ Main.py
@@ -3,13 +3,13 @@ from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtCore import Qt, pyqtSlot
 from interface.table_view import TableModel
 import sys
-import pandas as pd
 import json
+import pandas as pd
+from datetime import datetime
 from control.json_operations import set_dimension, get_dimension, set_current_page, get_last_page
+from control.pd_operations import return_df, transform_df
 
 main_dialog = uic.loadUiType("interface/main.ui")[0]
-
-# self.tableView.setModel(TableModel(df))
 
 
 class MyWindowClass(QtWidgets.QMainWindow, main_dialog):
@@ -32,13 +32,38 @@ class MyWindowClass(QtWidgets.QMainWindow, main_dialog):
         self.actionOptions.triggered.connect(
             lambda: self.grpMenu.setVisible(self.actionOptions.isChecked()))
         self.actionSave_last_page.triggered.connect(self.set_last_page_index)
+        # Set up words buttons
+        self.buttons = [self.btnWord1, self.btnWord2,
+                        self.btnWord3]
+        for index, btn in enumerate(self.buttons):
+            btn.clicked.connect(self.btn_random_word)
+            btn.setObjectName(str(index))
+
+        # Btn
+        #self.btnStart.clicked.connect(lambda: print("Start btn"))
+
+        # Input
+        self.txtDisplaySearch.textChanged.connect(
+            self.txt_search_input_changed)
         # DataFrame setup
-        self.df = pd.read_csv('control/Data.csv')
-        self.tableView.setModel(TableModel(self.df))
+        self.df = return_df('control/Data.csv')
+        self.set_table_model(self.df)
 
         # Another
         self.load_dimensions()
         self.load_last_page_index()
+
+    # SETTINGS
+    def set_table_model(self, _df: pd.DataFrame):
+        self.tableView.setModel(TableModel(_df))
+
+    def refresh_df(self, text: str):
+        tmp_df = transform_df(self.df, text)
+        self.set_table_model(tmp_df)
+
+    def txt_search_input_changed(self):
+        self.refresh_df(self.txtDisplaySearch.text())
+        print(self.txtDisplaySearch.text())
 
     def current_page_index(self) -> int:
         return self.stackedWidget.currentIndex()
@@ -77,7 +102,41 @@ class MyWindowClass(QtWidgets.QMainWindow, main_dialog):
         print("Wymiary")
         print(wymiary)
 
+    # Random words key pressed
+    def keyPressEvent(self, e):
+        if e.key() == Qt.Key_1:
+            self.buttons[0].click()
+        if e.key() == Qt.Key_2:
+            self.buttons[1].click()
+        if e.key() == Qt.Key_3:
+            self.buttons[2].click()
+
     # Btn configurations
+    def btn_random_word(self):
+        obj = self.sender().objectName()
+        value = self.progressBar.value() + 10
+        self.progressBar.setValue(value)
+        if value == 100:
+            self.result_setup()
+        print(f'value: {value}')
+        print(obj)
+
+    def result_setup(self):
+        self.stackedWidget.setCurrentWidget(self.StatsPage)
+        self.lblResult.setText(f"{self.correct_words},\n {self.bad_words}")
+        result_time = int((datetime.today() - self.start_time).total_seconds())
+        self.btnTimerResult.setText(f'Competition took {result_time} seconds')
+
+    @pyqtSlot()
+    def on_btnStart_clicked(self):
+        # set scores
+        self.correct_words = [f'good{i}' for i in range(10)]
+        self.bad_words = [f'bad{i}' for i in range(10)]
+        self.start_time = datetime.today()
+        self.btnStart.setEnabled(False)
+        self.btnStart.setStyleSheet(
+            "QPushButton""{""background-color : rgb(209,245,255);color:rgb(0,0,0)""}")
+        print("start")
 
     @pyqtSlot()
     def on_btnMain_clicked(self):
